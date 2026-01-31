b2ecd1f32be6b669edaeff61b5f54d09944f2614
Samarth Kavthale <arveix20@gmail.com>
2026-01-31 09:27:11 +0530

    fixed typos

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -24,7 +24,7 @@ function ProductGrid() {
     return(
         <>
             <section className="mx-auto w-full py-8">
-                <div className="grid grid-col-2 sm:grid-col-3 lg:grid-col-4 grid-row-auto gap-6">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 grid-row-auto gap-6">
                     {/* {
                         products.map((product) => {
                             return <ProductCard key={product.name} name={product.name} price={product.price} stock={product.stock}/>
