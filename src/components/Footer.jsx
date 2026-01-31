6cf891ef26af1856a0c6746df81b1b4bf5454782
Samarth Kavthale <arveix20@gmail.com>
2026-01-31 13:55:10 +0530

    minor changes to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center bg-black">
             <h1 className="text-5xl font-white text-bold">Footer</h1>
         </div>
     )
