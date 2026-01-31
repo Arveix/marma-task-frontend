41a4138433efb62c780bb584e766977a0ae5d64b
Samarth Kavthale <arveix20@gmail.com>
2026-01-31 14:17:09 +0530

    minor changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ function Header() {
 
     const languages = ["EN", "MR", "HI"];
     return (
-        <header className="bg-white border-b border-gray-200 p-4">
+        <header className="bg-white border-b border-gray-200 py-4 px-2">
             <div className="max-w-7xl mx-auto px-4 h-16 grid grid-cols-3 items-center">
 
                 <div className="flex items-center gap-3">
